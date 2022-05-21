4f011c82203adb5e37ceb9567e7afe61940d00cd
mrmikopu <mikailkkulak@hotmail.com>
2022-05-21 19:46:24 +0300

    Python IO example file

---
+++ Python_IO_Example.py
@@ -0,0 +1,207 @@
+#Run this prior to starting the exercise
+from random import randint as rnd
+
+memReg = 'members.txt'
+exReg = 'inactive.txt'
+fee =('yes','no')
+
+def genFiles(current,old):
+    with open(current,'w+') as writefile: 
+        writefile.write('Membership No  Date Joined  Active  \n')
+        data = "{:^13}  {:<11}  {:<6}\n"
+
+        for rowno in range(20):
+            date = str(rnd(2015,2020))+ '-' + str(rnd(1,12))+'-'+str(rnd(1,25))
+            writefile.write(data.format(rnd(10000,99999),date,fee[rnd(0,1)]))
+
+
+    with open(old,'w+') as writefile: 
+        writefile.write('Membership No  Date Joined  Active  \n')
+        data = "{:^13}  {:<11}  {:<6}\n"
+        for rowno in range(3):
+            date = str(rnd(2015,2020))+ '-' + str(rnd(1,12))+'-'+str(rnd(1,25))
+            writefile.write(data.format(rnd(10000,99999),date,fee[1]))
+
+
+genFiles(memReg,exReg)
+
+# This part implements the method.
+'''
+The two arguments for this function are the files:
+    - currentMem: File containing list of current members
+    - exMem: File containing list of old members
+    
+    This function should remove all rows from currentMem containing 'no' 
+    in the 'Active' column and appends them to exMem.
+    '''
+def cleanFiles(currentMem, exMem):
+    activeList = []
+    newList = []
+    # TODO: Open the currentMem file as in r+ mode
+    with open(currentMem, 'r+') as file1:
+        #TODO: Open the exMem file in a+ mode
+        with open(exMem,'a+') as file2:
+        #TODO: Read each member in the currentMem (1 member per row) file into a list.
+        # Hint: Recall that the first line in the file is the header.
+            activeList = file1.readlines()
+            newList = activeList[:]
+            for i,line in enumerate(activeList[1:]):
+                if(line[28:30] == 'no'):
+                    file2.write(line)
+                    del newList[newList.index(line)]
+                    
+        file1.seek(0,0)
+        file1.truncate()
+        file1.writelines(newList)
+
+memReg = 'members.txt'
+exReg = 'inactive.txt'
+cleanFiles(memReg,exReg)
+
+
+headers = "Membership No  Date Joined  Active  \n"
+with open(memReg,'r') as readFile:
+    print("Active Members: \n\n")
+    print(readFile.read())
+    
+with open(exReg,'r') as readFile:
+    print("Inactive Members: \n\n")
+    print(readFile.read())
+                
+# This part is a test for implementation
+  
+'''
+The two arguments for this function are the files:
+    - currentMem: File containing list of current members
+    - exMem: File containing list of old members
+    
+    This function should remove all rows from currentMem containing 'no' 
+    in the 'Active' column and appends them to exMem.
+    '''
+def cleanFiles(currentMem, exMem):
+    activeList = []
+    newList = []
+    # TODO: Open the currentMem file as in r+ mode
+    with open(currentMem, 'r+') as file1:
+        #TODO: Open the exMem file in a+ mode
+        with open(exMem,'a+') as file2:
+        #TODO: Read each member in the currentMem (1 member per row) file into a list.
+        # Hint: Recall that the first line in the file is the header.
+            activeList = file1.readlines()
+            newList = activeList[:]
+            for i,line in enumerate(activeList[1:]):
+                if(line[28:30] == 'no'):
+                    file2.write(line)
+                    del newList[newList.index(line)]
+                    
+        file1.seek(0,0)
+        file1.truncate()
+        file1.writelines(newList)
+
+memReg = 'members.txt'
+exReg = 'inactive.txt'
+cleanFiles(memReg,exReg)
+
+
+headers = "Membership No  Date Joined  Active  \n"
+with open(memReg,'r') as readFile:
+    print("Active Members: \n\n")
+    print(readFile.read())
+    
+with open(exReg,'r') as readFile:
+    print("Inactive Members: \n\n")
+    print(readFile.read())
+                
+
+# This part is verifies the implementation
+def testMsg(passed):
+    if passed:
+       return 'Test Passed'
+    else :
+       return 'Test Failed'
+
+testWrite = "testWrite.txt"
+testAppend = "testAppend.txt" 
+passed = True
+
+genFiles(testWrite,testAppend)
+
+with open(testWrite,'r') as file:
+    ogWrite = file.readlines()
+
+with open(testAppend,'r') as file:
+    ogAppend = file.readlines()
+
+try:
+    cleanFiles(testWrite,testAppend)
+except:
+    print('Error')
+
+with open(testWrite,'r') as file:
+    clWrite = file.readlines()
+
+with open(testAppend,'r') as file:
+    clAppend = file.readlines()
+        
+# checking if total no of rows is same, including headers
+
+if (len(ogWrite) + len(ogAppend) != len(clWrite) + len(clAppend)):
+    print("The number of rows do not add up. Make sure your final files have the same header and format.")
+    passed = False
+    
+for line in clWrite:
+    if  'no' in line:
+        passed = False
+        print("Inactive members in file")
+        break
+    else:
+        if line not in ogWrite:
+            print("Data in file does not match original file")
+            passed = False
+print ("{}".format(testMsg(passed)))
+    
+
+# This is a recommended implementation
+def cleanFiles(currentMem,exMem):
+    with open(currentMem,'r+') as writeFile: 
+        with open(exMem,'a+') as appendFile:
+            #get the data
+            writeFile.seek(0)
+            members = writeFile.readlines()
+            #remove header
+            header = members[0]
+            members.pop(0)
+                
+            inactive = [member for member in members if ('no' in member)]
+            '''
+            The above is the same as 
+
+            for member in members:
+            if 'no' in member:
+                inactive.append(member)
+            '''
+            #go to the beginning of the write file
+            writeFile.seek(0) 
+            writeFile.write(header)
+            for member in members:
+                if (member in inactive):
+                    appendFile.write(member)
+                else:
+                    writeFile.write(member)      
+            writeFile.truncate()
+                
+memReg = 'members.txt'
+exReg = 'inactive.txt'
+cleanFiles(memReg,exReg)
+
+# code to help you see the files
+
+headers = "Membership No  Date Joined  Active  \n"
+
+with open(memReg,'r') as readFile:
+    print("Active Members: \n\n")
+    print(readFile.read())
+    
+with open(exReg,'r') as readFile:
+    print("Inactive Members: \n\n")
+    print(readFile.read())
